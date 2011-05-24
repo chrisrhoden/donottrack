c12f56411ffdefa0857aa723a243784ff031974a
Chris Rhoden <carhoden@gmail.com>
2011-05-24 13:08:53 -0700

    Unnecessary

--- lib/donottrack.rb
+++ lib/donottrack.rb
@@ -1,14 +1,12 @@
 module Rack
   class DoNotTrack
-    DEFAULT_DNT_BIT = 0
-
     def initialize(app, dnt_bit = nil)
       @app = app
       @dnt_bit = dnt_bit
     end
 
     def call(env)
-      env['HTTP_DNT'] = @dnt_bit ? @dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || DEFAULT_DNT_BIT)
+      env['HTTP_DNT'] = @dnt_bit ? @dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || 0)
       @app.call(env)
     end
   end
