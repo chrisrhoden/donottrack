e9afdfcf3267f1f39ccf66f6057e79c35df0d7cb
Chris Rhoden <carhoden@gmail.com>
2011-05-24 13:17:28 -0700

    No Instance Variables!

--- lib/donottrack.rb
+++ lib/donottrack.rb
@@ -1,15 +1,10 @@
 module Rack
-  class DoNotTrack
-    DEFAULT_DNT_BIT = 0
-
-    def initialize(app, dnt_bit = nil)
-      @app = app
-      @dnt_bit = dnt_bit
-    end
-
-    def call(env)
-      env['HTTP_DNT'] = @dnt_bit ? @dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || DEFAULT_DNT_BIT)
-      @app.call(env)
+  module DoNotTrack
+    def self.new(app, dnt_bit=nil)
+      Proc.new do |env| 
+        env['HTTP_DNT'] = dnt_bit ? dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || 0)
+        app.call(env)
+      end
     end
   end
 end
