55b2a35ed8b4625bfd7d61e27d97518d72773568
Chris Rhoden <carhoden@gmail.com>
2011-05-24 13:03:32 -0700

    Massive indirection! Let's make this difficult to read.

--- lib/donottrack.rb
+++ lib/donottrack.rb
@@ -1,15 +1,10 @@
 module Rack
-  class DoNotTrack
+  class DoNotTrack < Struct.new(:app, :dnt_bit)
     DEFAULT_DNT_BIT = 0
 
-    def initialize(app, dnt_bit = nil)
-      @app = app
-      @dnt_bit = dnt_bit
-    end
-
     def call(env)
-      env['HTTP_DNT'] = @dnt_bit ? @dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || DEFAULT_DNT_BIT)
-      @app.call(env)
+      env['HTTP_DNT'] = self[:dnt_bit] ? self[:dnt_bit] : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || DEFAULT_DNT_BIT)
+      self[:app].call(env)
     end
   end
 end
