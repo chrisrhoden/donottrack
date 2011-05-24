3a5a906cc5dfc10306b2d2b3ada7b0733d16f4d9
Chris Rhoden <carhoden@gmail.com>
2011-05-24 13:26:21 -0700

    Better

--- lib/donottrack.rb
+++ lib/donottrack.rb
@@ -1,7 +1,5 @@
-module Rack
-  module DoNotTrack
-    def self.new(app, dnt_bit=nil)
-      Proc.new {|env| app.call(env.merge('HTTP_DNT'=> dnt_bit ? dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || 0)))}
-    end
+module Rack::DoNotTrack
+  def self.new(app, dnt_bit=nil)
+    Proc.new {|env| app.call(env.merge('HTTP_DNT'=> dnt_bit ? dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || 0)))}
   end
 end
