b43827bca3456bef9c9150d834bf9aa3d97425fe
Chris Rhoden <carhoden@gmail.com>
2011-05-24 13:08:22 -0700

    Default should override even if false

--- lib/donottrack.rb
+++ lib/donottrack.rb
@@ -8,7 +8,7 @@ module Rack
     end
 
     def call(env)
-      env['HTTP_DNT'] = @dnt_bit ? @dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || DEFAULT_DNT_BIT)
+      env['HTTP_DNT'] = @dnt_bit.nil? ? @dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || DEFAULT_DNT_BIT)
       @app.call(env)
     end
   end
